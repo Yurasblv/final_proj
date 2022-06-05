e09c4c593a8e26e742ebc2d47623f48b57dd7d75
yurasblv <yurasblv.y@gmail.com>
2022-06-05 17:56:46 +0300

    finish user

--- main/domain/crudbase.py
+++ main/domain/crudbase.py
@@ -1,5 +1,4 @@
-from typing import Generic, Type, TypeVar, Union, Any, Dict, List, Optional
-from flask.json import JSONEncoder
+from typing import Generic, Type, TypeVar, Any, List, Optional
 from pydantic import BaseModel
 from main.models import db
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
@@ -26,37 +25,13 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         self.model = model
 
     def get(self, db_: db, id_: Any) -> Optional[ModelType]:
-        return db_.query(self.model).filter(self.model.id == id_).first()
+        return db_.session.query(self.model).filter(self.model.id == id_).first()
 
     def get_multi(self, db_: db.session, *, skip: int = 0, limit: int = 10) -> List[ModelType]:
-        return db_.query(self.model).offset(skip).limit(limit).all()
-
-    def create(self, db_: db.session, *, obj_in: CreateSchemaType) -> ModelType:
-        obj_in_data = JSONEncoder().encode(obj_in)
-        db_obj = self.model(**obj_in_data)  # type: ignore
-        db_.session.add(db_obj)
-        db_.session.commit()
-        return db_obj
-
-    def update(self,
-               db_: db.session,
-               *,
-               db_obj: ModelType,
-               obj_in: Union[UpdateSchemaType, Dict[str, Any]]
-               ) -> ModelType:
-        obj_data = JSONEncoder().encode(db_obj)
-        if isinstance(obj_in, dict):
-            update_data = obj_in
-        else:
-            update_data = obj_in.dict(exclude_unset=True)
-        for field in obj_data:
-            setattr(db_obj, field, update_data[field])
-        db_.session.add(db_obj)
-        db_.session.commit()
-        return db_obj
+        return db_.session.query(self.model).offset(skip).limit(limit).all()
 
     def remove(self, db_: db.session, *, id_: int) -> ModelType:
-        obj = db_.query(self.model).get(id_)
+        obj = db_.session.query(self.model).get(id_)
         db_.session.delete(obj)
         db_.session.commit()
         return obj
--- main/domain/domains_func/user_create.py
+++ main/domain/domains_func/user_create.py
@@ -1,15 +1,16 @@
 from main.domain.user_repo import user_repo, admin_repo
-from main.models import db, User
+from main.models import db
 from main.shemas.user_shema import UserSchema, UserAdminSchema
+from typing import Optional
 
 
-def user_create(data) -> User:
+def user_create(data) -> Optional[UserSchema]:
     schema = UserSchema(**data)
-    user = user_repo.register_user(db, obj_in=schema)
-    return user
+    user_repo.register_user(db, obj_in=schema)
+    return schema
 
 
-def admin_create(data) -> User:
+def admin_create(data) -> Optional[UserAdminSchema]:
     schema = UserAdminSchema(**data)
-    admin = admin_repo.register_admin(db, obj_in=schema)
-    return admin
+    admin_repo.register_admin(db, obj_in=schema)
+    return schema

af8ff8f4cc28b61726eeda2f2ae592730bb91195
yurasblv <yurasblv.y@gmail.com>
2022-06-05 19:54:44 +0300

    repo created

--- main/shemas/film_shema.py
+++ main/shemas/film_shema.py
@@ -3,9 +3,10 @@ from pydantic import BaseModel, conint
 from datetime import date
 
 
-class Film(BaseModel):
+class FilmSchema(BaseModel):
     film_name = str
     movie_description = str
     premier_date = date
     rate = conint(ge=0, le=10)
     poster = Optional[str]
+
